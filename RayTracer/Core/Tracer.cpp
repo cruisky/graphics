14b4f7cf02f9175456859e43107ffe4aa298b258
cruisky <cruisky@gmail.com>
2015-04-16 00:55:53 -0400

    simple tracer

--- RayTracer/Core/Tracer.cpp
+++ RayTracer/Core/Tracer.cpp
@@ -1,13 +1,73 @@
 #include "stdafx.h"
 
 #include "Tracer.h"
+#include "Scene.h"
+#include "Intersection.h"
+#include "BSDF.h"
+#include "Color.h"
+#include "Ray.h"
 
 namespace Cruisky
 {
 	namespace RayTracer
 	{
-		void Tracer::RecursiveTrace(const Scene *scene, const Ray& ray, Color* color, int depth){
+		Color Tracer::RecursiveTrace(const Ray& ray, int depth){
+			if (depth < 0)
+				return Color::BLACK;
+			LocalGeo geo;
+			//Ray reflected, refracted;
+			Color color;
+			if (scene_->Intersect(ray, geo)){
+				scene_->PostIntersect(geo);
+				Shade(ray, geo, &color);
+
+				//if (Reflect(ray, geo, &reflected))
+				//	color += TraceReflection(reflected, geo, depth - 1);
+				//if (Refract(ray, geo, &refracted))
+				//	color += TraceRefraction(refracted, geo, depth - 1);
+			}
+			return color;
+		}
+
+		bool Tracer::Reflect(const Ray& ray, const LocalGeo& geo, Ray *reflected){
+			reflected->dir = ray.dir + 2.f * Math::Abs(Dot(ray.dir, geo.normal)) * geo.normal;
+			reflected->origin = geo.point + Ray::EPSILON * reflected->dir;
+			return true;
+		}
+
+		bool Tracer::Refract(const Ray& ray, const LocalGeo& geo, Ray *refracted){
+			const Vector3& d = ray.dir;
+			const Vector3& n = geo.normal;
+			float dn = Dot(d, n);
+			float iorinv = geo.bsdf->RefractiveIndexInv();
+			assert(iorinv != BSDF::NOT_REFRACTIVE);
+			float emergeAngle = 1.f - (1.f - dn * dn) * (iorinv * iorinv);
+			if (emergeAngle < 0)
+				return false;
+			refracted->dir = iorinv * (d + Math::Abs(dn) * n) - Math::Sqrt(emergeAngle) * n;
+			refracted->origin = geo.point + Ray::EPSILON * refracted->dir;
+			return true;
+		}
+
+		Color Tracer::TraceReflection(const Ray& reflected, const LocalGeo& geo, int depth){
 			//TODO
+		}
+
+		Color Tracer::TraceRefraction(const Ray& reflected, const LocalGeo& geo, int depth){
+			//TODO
+		}
+
+		void Tracer::Shade(const Ray& ray, const LocalGeo& geo, Color *out){
+			Color lightcolor;
+			Ray lightray;
+			const int lightcount = scene_->lights.size();
+			const Vector3& wo = -ray.dir;		// dir to camera
+			for (int i = 0; i < lightcount; i++){
+				scene_->lights[i]->Emit(geo, &lightray, &lightcolor);
+				if (!scene_->Occlude(lightray)){
+					*out += geo.bsdf->Eval(lightray.dir, wo, geo) * lightcolor;
+				}
+			}
 		}
 	}
 }
--- RayTracer/Core/Tracer.h
+++ RayTracer/Core/Tracer.h
@@ -1,23 +1,33 @@
 #pragma once
 #include "fwddecl.h"
 
+#include "Color.h"
+
 namespace Cruisky{
 	namespace RayTracer{
 		class Tracer {
 		public:
 			Tracer(int maxdepth = 5) : maxdepth_(maxdepth){}
+			virtual ~Tracer(){}
 
-			inline void Trace(const Scene *scene, const Ray& ray, Color *color){
-				RecursiveTrace(scene, ray, color, maxdepth_);
+			inline void Trace(const Ray& ray, Color *color){
+				*color = RecursiveTrace(ray, maxdepth_);
+				color->Clamp();
 			}
 
+			inline void SetScene(const Scene *scene){ scene_ = scene; }
+
 		protected:
-			virtual void RecursiveTrace(const Scene *scene, const Ray& ray, Color* color, int depth);
-			void Reflect(const Ray& ray, const LocalGeo& geo, Ray& reflected);
-			void Refract(const Ray& ray, const LocalGeo& geo, Ray& refracted);
-		
+			virtual Color RecursiveTrace(const Ray& ray, int depth);
+			inline bool Reflect(const Ray& ray, const LocalGeo& geo, Ray *reflected);
+			inline bool Refract(const Ray& ray, const LocalGeo& geo, Ray *refracted);
+			inline Color TraceReflection(const Ray& reflected, const LocalGeo& geo, int depth);
+			inline Color TraceRefraction(const Ray& refracted, const LocalGeo& geo, int depth);
+			inline void Shade(const Ray& ray, const LocalGeo& geo, Color *out);
+
 		private:
 			int maxdepth_;
+			const Scene *scene_;
 		};
 	}
 }
