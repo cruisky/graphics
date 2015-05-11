2dff162f1be3045c44ab6c16f02534c93232cc4b
cruisky <cruisky@gmail.com>
2015-05-11 01:32:32 -0400

    fix world-local transformations

--- RayTracer/Core/Intersection.h
+++ RayTracer/Core/Intersection.h
@@ -26,14 +26,14 @@ namespace Cruisky{
 			inline Vector3 WorldToLocal(const Vector3& vec) const {
 				assert(u != v);
 				return Vector3(
-					vec.x * u.x + vec.y * v.x + vec.z * normal.x,
-					vec.x * u.y + vec.y * v.y + vec.z * normal.y,
-					vec.x * u.z + vec.y * v.z + vec.z * normal.z);
+					vec.x * u.x + vec.y * u.y + vec.z * u.z,
+					vec.x * v.x + vec.y * v.y + vec.z * v.z,
+					vec.x * normal.x + vec.y * normal.y + vec.z * normal.z);
 			}
 
 			inline Vector3 LocalToWorld(const Vector3& vec) const {
 				assert(u != v);
-				return Vector3(Dot(vec, u), Dot(vec, v), Dot(vec, normal));
+				return vec.x * u + vec.y * v + vec.z * normal;
 			}
 		public:
 			const BSDF *bsdf;
